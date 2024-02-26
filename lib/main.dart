7cdf41839a80570400f33f5cb0a65bdfdfd403ac
hipposwagbg <hippo.gillet@hotmail.fr>
2024-02-26 12:05:18 +0100

    First Navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,79 @@
+import 'package:comic_vine/pages/comics_page.dart';
+import 'package:comic_vine/pages/film_page.dart';
 import 'package:comic_vine/pages/home_page.dart';
+import 'package:comic_vine/pages/search_page.dart';
+import 'package:comic_vine/pages/series_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0;
+
+  setCurrentIndex(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: [
+            HomePage(),
+            ComicsPage(),
+            FilmPage(),
+            SearchPage(),
+            SeriesPage()
+          ][_currentIndex],
+          bottomNavigationBar: NavigationBarTheme(
+            data: NavigationBarThemeData(
+              labelTextStyle: MaterialStateProperty.all(
+                TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              backgroundColor: Colors.white,
+              indicatorColor: Colors.transparent,
+            ),
+            child: NavigationBar(
+              selectedIndex: _currentIndex,
+              onDestinationSelected: setCurrentIndex,
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.home),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.book),
+                  label: 'Comics',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.movie),
+                  label: 'Film',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.search),
+                  label: 'Search',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.tv),
+                  label: 'Series',
+                ),
+              ],
+            ),
+        )));
   }
 }
